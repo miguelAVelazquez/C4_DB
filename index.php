679b13ff9767483fd66a229ee38b47fba52cb5ab
Reynald0 <reynaldobernard15@gmail.com>
2015-06-10 20:43:56 -0500

    Recargo dinamico

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <html>
 	<head>
 		<title>Menu inicio</title>
+		<link href="css/sitio.css" rel="stylesheet"/>
+		<!-- INICIO Javascript recargar una seccion de pagina -->
+		<script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
+		<script>
+		$(document).ready(function() {
+    		$('#etc_item').click(function(){
+       		$("#cuerpo").load("conexion.php");
+    											});
+
+    		$('#x').click(function(){
+       		$("#cuerpo").load("{% static 'index.html' %}");
+    											});
+
+									});
+		</script>
+		<!-- FIN Javascript recargar una seccion de pagina -->
 	</head>
 	<body>
 		<div>
 			<fieldset>
-				<legend>Datos recuperados: </legend>
+				<legend>Inicio: </legend>
 				<div>
-				Elige una opcion:
-				<a href="etc_item.php">etc_item</a>
-				
+					Elige una opcion:
+					<br>
+					<a href="#" id="etc_item">etc_item</a>
+					<table border ="1" id="cuerpo">
+					</table>
 				</div>
 			</fieldset>
 		</div>
