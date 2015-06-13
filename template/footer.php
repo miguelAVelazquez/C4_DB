00ccf84d0ffb248371a910a09ae136c7a35a004a
Reynald0 <reynaldobernard15@gmail.com>
2015-06-12 21:41:04 -0500

    Maquillaje - V2
    ** Se fija la barra de navegación en la parte superior y siempre se
    mantiene así cuando de baja en la página.
    
    ** Botón "ir arriba" dinámico y animado cada vez que se desciende en la
    página.

--- template/footer.php
+++ template/footer.php
@@ -3,5 +3,33 @@
 
 <script src="js/bootstrap.min.js"></script>
 <!-- Abajo de aqui poner los script JS propios -->
+<div id='IrArriba'>
+<a href='#Arriba'><span/></a>
+</div>
+<script type='text/javascript'>
+	//
+	// Botón para Ir Arriba
+	jQuery.noConflict();
+	jQuery(document).ready(function() {
+		jQuery("#IrArriba").hide();
+		jQuery(function () {
+			jQuery(window).scroll(function () {
+			if (jQuery(this).scrollTop() > 200) {
+				jQuery('#IrArriba').fadeIn();
+			} 
+			else {
+				jQuery('#IrArriba').fadeOut();
+			}
+			});
+			jQuery('#IrArriba a').click(function () {
+				jQuery('body,html').animate({
+				scrollTop: 0
+				}, 800);
+				return false;
+			});
+		});
+
+	});
+</script>
 </body>
 </html>
