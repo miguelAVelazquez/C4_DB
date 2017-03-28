0492669db2c6218a8ffc5e6c2bebc6b65734ba4b
Reynald0 <reynaldobernard15@gmail.com>
2017-03-28 11:03:39 -0600

    .

--- es/template/navbar.php
+++ es/template/navbar.php
@@ -26,13 +26,13 @@
                         <li><a href="index">Español</a></li>
                   </ul>
                 </li>
-                <li><a href="http://www.l2devsadmins.com/">Foro</a></li>
-                <li><a href="http://devsc6.hol.es/">C6 DB<span class="label label-info">New</span></a></li>
+                <li><a href="http://l2devsadmins.net/l2devsAdmins2-0-11/">Foro</a></li>
+                <!--li><a href="http://devsc6.hol.es/">C6 DB<span class="label label-info">New</span></a></li-->
             </ul>
-            <ul class="nav navbar-nav navbar-right">
+            <!--ul class="nav navbar-nav navbar-right">
                 <li><img src="../img/c4_logo.png"/></li>
                 <li><img src="../img/devsadmins.png"/></li>
-            </ul>
+            </ul-->
 
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
